b554647c2ec741c9b890c5da1b1db4b81415a324
mechanic <ornobtanvir.git@gmail.com>
2019-12-28 14:29:48 +0600

    added

--- Custom_login_and_reg/app/Http/Controllers/AuthController.php
+++ Custom_login_and_reg/app/Http/Controllers/AuthController.php
@@ -1,33 +1,85 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 use Illuminate\Http\Request;
+use Validator,Redirect,Response;
+Use \App\User;
+use Session;
+
 
 class AuthController extends Controller
 {
     public function index(){
-        return "index";
+        
+        return view('login');
+        // sending the interface for login
     }
 
     public function postLogin(Request $request){
-        return 'post login';
+        
+        // take the whole login data 
+        // and pass it to the validator
+        //get the data
+        // $data = $request->all();
+        // return Response()->json($data);
+        // but we will only take the email and password
+        request()->validate([
+            'email' => 'required',
+            'password'=>'required',
+        ]);
+        $credential = $request->only('email','password');
+        if(Auth::attempt($credential)){
+            return redirect()->intended('dashboard');
+        }
+        return Redirect::to('login')->withSuccess('wrong Credential');
     }
 
     public function registration(){
-        return "registration";
+        return view('registration');
     }
 
-    public function postRegistration(){
-        return "post registration";
+    public function postRegistration(Request $request){
+        // add the validation
+        // we pass the request with a middleware
+        // called validate
+        request()->validate([
+            'name' =>'required',
+            'email' => 'required',
+            'password'=>'required| min:3',
+        ]);
+        // in error this will create the 
+        // error array and send it to the 
+        // registration.blade.php 
+
+
+        $data = $request->all();
+        // remember you must must hash your password
+        // other wise the authentication wont work
+        $user = new User;
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->password =Hash::make($data['password']);
+        $user->save();
+        //return Response()->json($data['email']);
+        //fetch all the data
+        //return Response()->json($data);
+        // if you do not hash your password this thing will never work
+        // User::create($data);
+        return Redirect::to('dashboard')->withSuccess('You are Registered');
     }
 
     public function dashboard(){
-        return "dash board";
+        return view('dashboard');
     }
 
 
     public function logout(){
-        return "logout";
-    }
+        // flush the session
+        Session::flush();
+        Auth::logout();
+        return Redirect::to('login'); 
+       }
 }
